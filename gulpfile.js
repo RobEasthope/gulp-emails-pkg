be87cd2db6c820472751eab8202861a34b3a8082
Rob Easthope <robeasthope@gmail.com>
2014-12-09 14:40:48 +0000

    Set up watch task and clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,17 @@
+// CONFIG
 // Dependencies
 var gulp = require('gulp');
+
+// Load config file
+var config = require('./config.json');
 
 // Skip loading each dependency individually and load via gulp-load-plugins task
 var gulpLoadPlugins = require('gulp-load-plugins');
 var browserSync = require('browser-sync');
 var plugins = gulpLoadPlugins();
 
-// Load config file
-var config = require('./config.json');
 
-// CSS COMPILE
-gulp.task('sass', function () {
-    gulp.src('./source/scss/_css-compile/*.scss')
-        .pipe(plugins.sass())
-        .pipe(gulp.dest('./source/css/'));
-});
-
-
-// HTML BUILD TASK
-gulp.task('build', ['sass', 'browser-sync'], function () {
-  gulp.src('./source/html/index.html')
-    .pipe(plugins.inline({
-      base: './',
-      css: plugins.minifyCss()
-    }))
-    .pipe(plugins.premailer())
-    .pipe(plugins.replace('{TEST}', 'SUCCESSFUL FULL BUILD TEST'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(plugins.filter('./build/*.html')) // Filtering stream to only html build files
-    .pipe(browserSync.reload({stream:true}));
-});
-
-
-// BROWSER-SYNC
+// Browser-sync settings
 gulp.task('browser-sync', function () {
    var files = [
       './build/*.html'
@@ -46,9 +25,32 @@ gulp.task('browser-sync', function () {
 });
 
 
+// CSS COMPILE
+gulp.task('sass', function () {
+    gulp.src('./source/scss/_css-compile/*.scss')
+        .pipe(plugins.sass())
+        .pipe(gulp.dest('./source/css/'));
+});
+
+
+// HTML BUILD TASK
+gulp.task('html-build', function () {
+  gulp.src('./source/html/index.html')
+    .pipe(plugins.inline({
+      base: './',
+      css: plugins.minifyCss()
+    }))
+    .pipe(plugins.premailer())
+    .pipe(plugins.replace('{TEST}', 'SUCCESSFUL FULL BUILD TEST'))
+    .pipe(gulp.dest('./build/'))
+});
+
+
 // WATCH TASK
-gulp.task('dev', ['sass', 'inline', 'premailer', 'browser-sync'], function() {
+gulp.task('dev', ['browser-sync'], function() {
     //a list of watchers, so it will watch all of the following files waiting for changes
-    gulp.watch('./source/scss/*.scss', ['sass', 'inline', 'premailer']);
-    gulp.watch('./source/html/*.html', ['inline', 'premailer']);
+    gulp.watch('./source/scss/*.scss', ['sass', 'html-build']);
+    gulp.watch('./source/html/*.html', ['html-build']);
+    .pipe(plugins.filter('./build/*.html')) // Filtering stream to only html build files
+    .pipe(browserSync.reload({stream:true}));
 });
